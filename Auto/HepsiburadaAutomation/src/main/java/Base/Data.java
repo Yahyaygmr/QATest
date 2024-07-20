69b03ffd81c9be99ef7a4a67a9c65959e736b8dc
Yahya Yağmur <yahyaygmr@gmail.com>
2024-07-20 12:06:35 +0300

    new pro

--- Auto/HepsiburadaAutomation/src/main/java/Base/Data.java
+++
@@ -1,8 +0,0 @@
-package Base;
-
-public class Data {
-    public static String email = "";
-    public static String password = "";
-    public static String errorMessage = "Kontrol edip tekrar deneyin.";
-    public static String testUrl="https://giris.hepsiburada.com";
-}
